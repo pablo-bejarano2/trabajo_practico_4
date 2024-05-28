7e5c8763b4bb5fe78c59e45cb0a776d8634e4a88
Pablo Bejarano <pablobejarano2112@gmail.com>
2024-05-28 20:34:01 -0300

    trabajo practico 4

--- src/main/java/ar/edu/unju/fi/model/Docente.java
+++ src/main/java/ar/edu/unju/fi/model/Docente.java
@@ -1,5 +1,9 @@
 package ar.edu.unju.fi.model;
 
 public class Docente {
-
+	private Integer legajo;
+	private String nombre;
+	private String apellido;
+	private String email;
+	private Integer telefono;
 }
